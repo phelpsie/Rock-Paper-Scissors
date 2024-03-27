d53b49aab6ca9a524cfe2fc300a75c5d20674718
charliephelpscodes <164572692+charliephelpscodes@users.noreply.github.com.>
2024-03-27 15:50:11 -0700

    Create getComputerChoice function which logs a number at random between 1 and 99, inclusively, to the console

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-
-
+function getComputerChoice() {
+    let min = 1;
+    let max = 99;
+    let num = Math.floor(Math.random() * (99)) + 1;
+    if (num < 33) {
+        console.log('rock');
+    } else if (num < 66) {
+        console.log('paper');
+    } else {
+        console.log('scissors');
+    }
+}
